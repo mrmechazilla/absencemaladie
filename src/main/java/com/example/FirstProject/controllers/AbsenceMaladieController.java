2aa06a0954f94fbd3667ea7f757f829df143a8cc
Lokmane Hsseine <lokmane.hsseine.1996@gmail.com>
2023-03-27 12:30:03 +0000

    test

--- src/main/java/com/example/FirstProject/controllers/AbsenceMaladieController.java
+++ src/main/java/com/example/FirstProject/controllers/AbsenceMaladieController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RequestMapping("/absences")
 public class AbsenceMaladieController {
 
+    //comentaire de test
     @Autowired
     private AbsenceMaladieService absenceMaladieService;
 
