f862a0985d5baba069f69a281076b98166fa3d07
Lokmane Hsseine <lokmane.hsseine.1996@gmail.com>
2023-03-27 14:49:18 +0000

    update nouhaila

---
+++ src/main/java/com/example/FirstProject/controllers/CollaborateurController.java
@@ -0,0 +1,29 @@
+package com.example.FirstProject.controllers;
+
+import com.example.FirstProject.entities.Collaborateur;
+import com.example.FirstProject.services.CollaborateurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/collaborateur")
+public class CollaborateurController {
+    @Autowired
+    private CollaborateurService collaborateurService;
+    @PostMapping("/addCollaborateur")
+    public String addCollaborateur(@RequestBody Collaborateur collaborateur){
+        collaborateurService.saveCollaborateur(collaborateur);
+        return "collaborateur enregistré avec succé";
+    }
+    @GetMapping("/allCollaborateur")
+    public List<Collaborateur> getAllCollaborateur(){
+        return collaborateurService.getAllCollaborateurs();
+    }
+    @GetMapping("/getCollaborateurById/{id}")
+    public Optional<Collaborateur> getCollaborateurById(@PathVariable Long id){
+        return collaborateurService.getCollaborateurById(id);
+    }
+}
